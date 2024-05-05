be6a8f69aa4eb9be8bc2dfbc685d188e361f364c
5Marq <epgawin@yahoo.pl>
2024-05-05 18:27:54 +0200

    animal repo done

--- tutorial5/Repos/IAnimalRepository.cs
+++ tutorial5/Repos/IAnimalRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace tutorial5.Repos;
+﻿using tutorial5.Models;
 
-public class IAnimalRepository
+namespace tutorial5.Repos;
+
+public interface IAnimalRepository
 {
-    
+    IEnumerable<Animal> GetAnimals();
+    int CreateAnimal(Animal animal);
+    Animal GetAnimal(int idAnimal);
+    int UpdateAnimal(Animal animal);
+    int DeleteAnimal(int idAnimal);
 }
---
+++ tutorial5/Services/AnimalService.cs
@@ -0,0 +1,45 @@
+﻿using GakkoHorizontalSlice.Model;
+using GakkoHorizontalSlice.Repositories;
+using tutorial5.Services;
+
+namespace GakkoHorizontalSlice.Services;
+
+public class Animal : IAnimalService
+{
+    private readonly IAnimalRepository _animalRepository;
+    
+    public StudentsService(IStudentsRepository studentsRepository)
+    {
+        _studentsRepository = studentsRepository;
+    }
+    
+    public IEnumerable<Student> GetStudents()
+    {
+        //Business logic
+        return _studentsRepository.GetStudents();
+    }
+    
+    public int CreateStudent(Student student)
+    {
+        //Business logic
+        return _studentsRepository.CreateStudent(student);
+    }
+
+    public Student? GetStudent(int idStudent)
+    {
+        //Business logic
+        return _studentsRepository.GetStudent(idStudent);
+    }
+
+    public int UpdateStudent(Student student)
+    {
+        //Business logic
+        return _studentsRepository.UpdateStudent(student);
+    }
+
+    public int DeleteStudent(int idStudent)
+    {
+        //Business logic
+        return _studentsRepository.DeleteStudent(idStudent);
+    }
+}
